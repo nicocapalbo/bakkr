176ed0501d51156c56ca85cfdf497c2a541e6694
Nico Capalbo <nicolas.capalbo@lystos.com>
2023-10-18 18:23:51 +0200

    minor config

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,10 @@ class Product < ApplicationRecord
   has_many :product_ingredients, dependent: :destroy
   has_many :ingredients, through: :product_ingredients
 
-  enum :product_type, cake: 0, brigadeiro: 1
+  enum product_type: {
+    cake: 0,
+    brigadeiro: 1
+  }
 
   # Add validations and associations as needed
   def cake?
